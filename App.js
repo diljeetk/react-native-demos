ff784de90a854e35e789d8788434b1742527be5a
diljeetkakkar <devdiljeetkakkar@gmail.com>
2018-03-28 16:11:04 +0530

    implemeted scrollView, ListView,fetchApi

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import {PizzaTranslator} from './translate.js';
+import {FlatListBasics} from './flatListBasics.js';
+import {SectionListBasics} from './sectionListBasics.js';
+import {FetchExample} from './fetchExample.js';
 
 export default class App extends React.Component {
   render() {
@@ -8,23 +11,32 @@ export default class App extends React.Component {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <View>
-         <PizzaTranslator />
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Greeting name='Diljeet' />
-        <Greeting name='Mr Kakkar' />
-        <Greeting name='Mr Diljeet Kakkar' />
-        <Blink text='I love to blink' />
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 20, height: 10, backgroundColor: 'steelblue'}} />
-        <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      <ScrollView>
+        <View>
+          <PizzaTranslator />
+          <Text style={{fontSize:50}}>Scroll me plz</Text>
+          <Image source={pic} style={{width: 193, height: 110}}/>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Greeting name='Diljeet' />
+          <Greeting name='Mr Kakkar' />
+          <Greeting name='Mr Diljeet Kakkar' />
+          <Blink text='I love to blink' />
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+          <View style={{width: 20, height: 10, backgroundColor: 'steelblue'}} />
+          <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+          </View>
+          <Text style={{fontSize:30}}>Flat List Basics</Text>
+          <FlatListBasics />
+          <Text style={{fontSize:30}}>Section List Basics</Text>
+          <SectionListBasics />
+          <Text style={{fontSize:30}}>Fetch API example</Text>
+          <FetchExample/>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
