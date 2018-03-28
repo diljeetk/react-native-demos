b6c8d0fd7ece7d8c0f2de74d6ede4e2563003a1a
diljeetkakkar <devdiljeetkakkar@gmail.com>
2018-03-28 12:58:08 +0530

    setting state in the component

--- App.js
+++ App.js
@@ -16,6 +16,10 @@ export default class App extends React.Component {
         <Greeting name='Diljeet' />
         <Greeting name='Mr Kakkar' />
         <Greeting name='Mr Diljeet Kakkar' />
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
 
       </View>
     );
@@ -27,6 +31,27 @@ class Greeting extends React.Component {
       <Text>Hello {this.props.name}!</Text>
     );
   }
+}
+
+class Blink extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {isShowingText: true};
+
+  // Toggle the state every second
+  setInterval(() => {
+    this.setState(previousState => {
+      return { isShowingText: !previousState.isShowingText };
+    });
+  }, 1000);
+}
+
+render() {
+  let display = this.state.isShowingText ? this.props.text : ' ';
+  return (
+    <Text>{display}</Text>
+  );
+}
 }
 
 const styles = StyleSheet.create({
